1886195229599f1f0d500c41b645b76a90ea34a9
Bachemedei <bronwyn.1992@gmail.com>
2020-09-05 13:32:05 +0800

    started whack-a-mole

--- Exercise3/whack-a-mole/script.js
+++ Exercise3/whack-a-mole/script.js
@@ -1,6 +1,18 @@
+function moleDisappear(mole) {
+  console.log("diappear");
+  mole.classList.toggle("mole-appear");
+}
+
 function moleAppear() {
-    var id = "mole" + 1
-    console.log(id)
-    mole = document.getElementById("mole1")
-    mole.classList.toggle('mole-appear')
+  console.log("appear");
+  var index = Math.round(Math.random() * 3);
+  mole = document.getElementsByClassName("mole")[index];
+  mole.classList.toggle("mole-appear");
+  setTimeout(() => {
+      moleDisappear(mole)
+  }, 3000);
+}
+
+function moleStart(){
+    setTimeout(moleAppear, 3000);
 }
