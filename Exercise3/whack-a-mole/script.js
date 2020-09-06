b975089531701be0a865ffd3d8bb952b446890a8
Bachemedei <bronwyn.1992@gmail.com>
2020-09-06 09:23:46 +0800

    whackamole working

--- Exercise3/whack-a-mole/script.js
+++ Exercise3/whack-a-mole/script.js
@@ -1,18 +1,54 @@
+var button = false;
+var score = 0
+var mole;
+var isMole = false
+
+window.onload = () => {
+  var holes = document.querySelectorAll("div.mole")
+  holes.forEach(hole => {
+    hole.addEventListener('click', changeScore)
+  })
+}
+
+function toggleButton() {
+  if (button === false) {
+    moleStart();
+  }
+  button = !button;
+}
+
 function moleDisappear(mole) {
-  console.log("diappear");
-  mole.classList.toggle("mole-appear");
+  mole.classList.toggle("mole-visible");
+  isMole = false
 }
 
 function moleAppear() {
-  console.log("appear");
+  isMole = true
   var index = Math.round(Math.random() * 3);
   mole = document.getElementsByClassName("mole")[index];
-  mole.classList.toggle("mole-appear");
+  mole.classList.toggle("mole-visible");
+
+  
+  var time = Math.round(Math.random() * 700) + 400;
   setTimeout(() => {
-      moleDisappear(mole)
-  }, 3000);
+    moleDisappear(mole);
+    if (button === true) {
+      moleStart();
+    }
+  }, time);
 }
 
-function moleStart(){
-    setTimeout(moleAppear, 3000);
+function moleStart() {
+  var time = Math.round(Math.random() * 2000) + 500;
+  setTimeout(moleAppear, time);
+}
+
+function changeScore(e){
+  var moleClicked = e.currentTarget.innerText
+  var moleActive = mole.innerText
+  if (moleClicked === moleActive && isMole === true) {
+    score++
+  }
+  currentScore = "Score: " + score
+  document.getElementById("current-score").innerText = currentScore
 }
