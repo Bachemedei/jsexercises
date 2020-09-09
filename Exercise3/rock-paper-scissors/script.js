5932d3ba553a06ce23e1f92f6168718e8f372405
Bachemedei <bronwyn.1992@gmail.com>
2020-09-09 22:36:33 +0800

    Finished rock paper scissors apart from a few tweaks needed for styling

--- Exercise3/rock-paper-scissors/script.js
+++ Exercise3/rock-paper-scissors/script.js
@@ -10,53 +10,83 @@ window.onload = () => {
   });
 };
 
-// Display the choices of the user and the computer
-function showChoice(event) {
-  userChoice = event.currentTarget.innerText;
-  document.getElementById("user-choice").innerText = userChoice;
-
-  computerSelection();
-  winner();
-}
-
 // Randomising the computer choice
 function computerSelection() {
   const choices = ["Rock!", "Paper!", "Scissors!"];
   var index = Math.round(Math.random() * 2);
   computerChoice = choices[index];
-  document.getElementById("computer-choice").innerText = computerChoice;
+  var container = "computer-img"
+  // display image
+  determineImageUrl(computerChoice, container)
+  //display choice text
+  document.getElementById("computer-choice").innerText = "The computer chose " + computerChoice;
 }
+
+// Get image URL based on choice and add to HTML
+function determineImageUrl(choice, container) {
+    // Determine which image URL to use
+    if (choice === "Rock!") {
+        imageURL = "https://ctl.s6img.com/society6/img/Ee7g71xE-r6Yg3B-M75Bt9nPtbg/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/1badeed855904fcc89c274af6161c983/~~/cute-rock-prints.jpg"
+    }
+    if (choice === "Paper!") {
+        imageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZQEvW75gxhCU6t_APVjrSSn6m0ypguCq-7Q&usqp=CAU"
+    }
+    if (choice === "Scissors!") {
+        imageURL = "https://cdn4.vectorstock.com/i/thumb-large/69/78/scissor-cute-cartoon-vector-14506978.jpg"
+    }
+    // Add image to the HTML doc
+    function addImage(container) {
+        var image = document.getElementById(container)
+        image.src = imageURL
+        // document.getElementById(container).appendChild(image)
+    }
+    addImage(container)
+}
+
+// Display the choices of the user and the computer
+function showChoice(event) {
+    userChoice = event.currentTarget.innerText;
+    var container = "user-img"
+    // display image
+    determineImageUrl(userChoice, container)
+    // display choice text
+    document.getElementById("user-choice").innerText = "You chose " + userChoice;
+    
+    // run computer selection
+    computerSelection();
+    winner();
+  }
 
 // Determining winner
 function winner() {
     if (userChoice === computerChoice) {
-        result = "Tie!"
+        result = "It's a tie. Try again"
     }
     if (userChoice === "Rock!") {
         console.log("User chose rock")
         if (computerChoice === "Scissors!") {
-            result = "You Win!"
+            result = "You win!"
         }
         if (computerChoice === "Paper!") {
-            result = "Computer Wins"
+            result = "Computer wins, try again"
         }
     }
     if (userChoice === "Scissors!") {
         console.log("User chose scissors")
         if (computerChoice === "Rock!") {
-            result = "Computer Wins"
+            result = "Computer wins, try again"
         }
         if (computerChoice === "Paper!") {
-            result = "You Win!"
+            result = "You win!"
         }
     }
     if (userChoice === "Paper!") {
         console.log("User chose paper")
         if (computerChoice === "Rock!") {
-            result = "You Win!"
+            result = "You win!"
         }
         if (computerChoice === "Scissors!") {
-            result = "Computer Wins"
+            result = "Computer wins, try again"
         }
     }
     document.getElementById("winner").innerText = result;
